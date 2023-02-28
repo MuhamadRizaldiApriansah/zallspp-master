b47631591f5e65a7e8b1516832a59ba27101ece7
MuhamadRizaldiApriansah <rizaldiapriansah9@gmail.com>
2023-02-28 13:49:31 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,21 @@
+@extends('welcome')
+@section('content')
+
+<!-- End Navbar -->
+
+<div class="page-header min-vh-100 align-center" style="height: 100%">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card text-center">
+                <img src="https://th.bing.com/th/id/R.70a40594d13b10617770b793181f8ba2?rik=Op5xIZ9hOzwjJQ&riu=http%3a%2f%2fwww.smkn4bogor.sch.id%2fwp-content%2fuploads%2f2020%2f07%2flogoSMKN4.jpg&ehk=ApMDWQ4YnUhlYNdjcTzhXYOFmQZLMYMeq2KGpmpcTG4%3d&risl=&pid=ImgRaw&r=0" class="text-center" style="width: 200px; margin:auto" alt="...">
+                <h1 class="mt-2">Aplikasi Pembayaran SPP Online</h1>
+                <hr class="my-4">
+                <p class="lead">Aplikasi berbasis website pembayaran spp online <br> SMKN 4 Kota Bogor.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
